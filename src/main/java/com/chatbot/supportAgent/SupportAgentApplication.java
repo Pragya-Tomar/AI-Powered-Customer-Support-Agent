dd28b1983fbe1f3ebdf89e8bcdc50549d7647add
Pragya Tomar <56336816+Pragya-Tomar@users.noreply.github.com>
2026-04-26 15:16:47 +0000

    add spring boot starter project

---
+++ src/main/java/com/chatbot/supportAgent/SupportAgentApplication.java
@@ -0,0 +1,13 @@
+package com.chatbot.supportAgent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupportAgentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SupportAgentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chatbot/supportAgent/SupportAgentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chatbot.supportAgent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupportAgentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
